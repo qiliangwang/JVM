38ec78109eea7402b1ad946305dc87c8d3efa08a
vaderwang <2953381201@qq.com>
2019-01-24 19:49:20 +0800

    change package

---
+++ monitor_tuning/src/main/java/com/imooc/tuning/chapter8/Singleton.java
@@ -0,0 +1,11 @@
+package com.imooc.tuning.chapter8;
+
+public class Singleton {
+	private Singleton() {}
+	private static class SingletonHolder{
+		private static Singleton instance = new Singleton();
+	}
+	public static Singleton getInstance() {
+		return SingletonHolder.instance;
+	}
+}
